b3b425e813154482706446e505acad6aa126fc28
Luiz Fabiano de Souza <luizfabianods04@gmail.com>
2025-02-18 17:46:34 -0300

    Atualização: Menu do Game, Script do Menu e Artes do Menu

---
+++ Scripts/Menu/menuprincipal.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class menuprincipal : MonoBehaviour
+{
+    public static menuprincipal instancia {get; private set;}
+    public GameObject opçao, credito, dlc;
+    
+
+ private void Awake()
+    {
+        if (instancia == null)
+            instancia = this;
+    }
+    void Start()
+    {
+          opçao.gameObject.SetActive(false);
+    credito.gameObject.SetActive(false);
+    dlc.gameObject.SetActive(false);
+    }
+
+    public void iniciar()
+  {
+     SceneManager.LoadScene(1);
+  }
+ public void opções()
+ {
+    opçao.gameObject.SetActive(true);
+ }
+ public void creditos()
+ {
+    credito.gameObject.SetActive(true);
+ }
+   public void sair()
+    {
+        Application.Quit();
+    }
+
+ public void maiscoisa()
+ {
+   dlc.gameObject.SetActive(true);
+ }
+
+ public void fecharJanela()
+ {
+    opçao.gameObject.SetActive(false);
+    credito.gameObject.SetActive(false);
+    dlc.gameObject.SetActive(false);
+
+
+
+
+ }
+}
